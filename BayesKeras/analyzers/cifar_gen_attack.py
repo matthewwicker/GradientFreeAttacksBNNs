d66564148d11de2ec81bfcb3807f2284c27a34ab
Matthew Yuan <matthewfelixyuan@gmail.com>
2020-11-08 21:39:24 +0000

    adapt gen attack for CIFAR

---
+++ BayesKeras/analyzers/cifar_gen_attack.py
@@ -0,0 +1,124 @@
+# Implementing a genetic algorithm to find adversarial examples.
+import sys
+from tqdm import tqdm
+sys.path.append('../')
+import numpy as np
+rng = np.random.default_rng()
+
+# Returns a length-n numpy array whose elements are all arr. A new object, not a reference.
+def multiply(arr, n):
+    return np.asarray(n*[arr[:]])
+
+# This is the only selection operator that is vectorized.
+def tournament2(f):
+    N = f.shape[1]
+    parents = np.zeros_like(f, dtype='int')
+    for i in range(N):
+        [c1, c2] = rng.choice(N, size=2)
+        fighters1 = f[:, c1]
+        fighters2 = f[:, c2]
+        battle = fighters1 > fighters2
+        parents[:, i] += c1*battle
+        parents[:, i] += c2*np.invert(battle)
+    return parents
+
+# This is the only crossover operator that is vectorized.
+def single2(pop, parents):  # pretty good
+    L = pop.shape[0]
+    N = pop.shape[1]
+    new_pop = np.zeros_like(pop)
+    for i in range(0, N, 2):
+        moms = parents[:, i]
+        dads = parents[:, i+1]
+        mom_squares = pop[range(L), moms]
+        dad_squares = pop[range(L), dads]
+        cross_point = rng.choice(32)
+        if rng.choice(2) == 0:
+            new_pop[:, i] = np.append(mom_squares[:, :cross_point, :, :], dad_squares[:, cross_point:, :, :], axis=1)
+            new_pop[:, i+1] = np.append(dad_squares[:, :cross_point, :, :], mom_squares[:, cross_point:, :, :], axis=1)
+        else:
+            new_pop[:, i] = np.append(mom_squares[:, :, :cross_point, :], dad_squares[:, :, cross_point:, :], axis=2)
+            new_pop[:, i+1] = np.append(dad_squares[:, :, :cross_point, :], mom_squares[:, :, cross_point:, :], axis=2)
+    return(new_pop)
+
+# This is the only mutation operator that is vectorized.
+def one_pixel(new_pop, R):  # pretty good with R = 0.5
+    mutated_pop = new_pop
+    N = new_pop.shape[1]
+    for i in range(N):
+        if rng.uniform() < R:
+            x, y = rng.choice(32, size=2)
+            mutated_pop[:, i, x, y] *= -1
+    return mutated_pop
+
+# Takes in a SINGLE input numpy array. For now, only an untargeted attack. Parameters are:
+# G: number of generations
+# R: mutation rate
+# N: population size
+# D: perturbation size
+def cifar_gen_attack(model, inp, G, R, N, D, selection='t2', crossover='s2', mutation='1p'):
+
+    true_classes = np.argmax(model.predict(inp, n=15), axis=1)
+    pop = rng.choice([-D, D], size=(inp.shape[0], N, inp.shape[1], inp.shape[2], inp.shape[3]))
+    # pop = rng.uniform(-D, D, size=(N, inp.shape[0]))
+    copies = np.zeros((inp.shape[0], N, inp.shape[1], inp.shape[2], inp.shape[3]))
+    for i in range(inp.shape[0]):
+        for j in range(N):
+            copies[i][j] += inp[i]
+    stagnation = 0
+    best_fitness_so_far = -1000
+
+    for g in tqdm(range(G)):
+        
+        # Compute fitness.
+        clipped = np.asarray(np.clip(copies + pop, 0, 1))
+        preds = np.asarray(model.predict(clipped.reshape((-1, inp.shape[1], inp.shape[2], inp.shape[3])), n=15)).reshape((inp.shape[0], N, -1))
+        true_softmaxes = preds[range(inp.shape[0]), :, true_classes]
+        f = -np.log(true_softmaxes)
+
+        # Return the fittest member if it misclassifies.
+        best = np.argmax(f, axis=1)
+        # if np.argmax(preds[best]) != true_class:
+            # print('noice!')
+            # return inp + pop[best]
+
+        # Do selection.
+        if selection == 't2':
+            parents = tournament2(f)
+        elif selection == 'prop':
+            parents = proportional(f)
+        elif selection == 'soft':
+            parents = soft(f)
+        elif selection == 'rank':
+            parents = rank(f)
+        else:
+            return 'invalid selection!'
+        
+        # Do crossover.
+        if crossover == 's':
+            new_pop = single(pop, parents)
+        elif crossover == 'd':
+            new_pop = double(pop, parents)
+        elif crossover == 's2':
+            new_pop = single2(pop, parents)
+        elif crossover == 'r':
+            new_pop = respectful(pop, parents)
+        else:
+            return 'invalid crossover!'
+
+        # Do mutation.
+        if mutation == 'rp':
+            new_pop = rate_pixel(new_pop)
+        elif mutation == '1p':
+            new_pop = one_pixel(new_pop, R)
+        elif mutation == '6p':
+            new_pop = six_pixel(new_pop)
+        elif mutation == '10p':
+            new_pop = ten_pixel(new_pop)
+        else:
+            return 'invalid mutation!'
+
+        pop = new_pop
+
+    return np.clip(inp + pop[range(inp.shape[0]), best], 0, 1)
+
--- CIFAR_Experiments/analysis.py
+++ CIFAR_Experiments/analysis.py
@@ -21,10 +21,14 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--opt")
 parser.add_argument("--arch")
-
+parser.add_argument("--attack")
+parser.add_argument("--G", type=int, nargs='?', const=-1)
+parser.add_argument("--R", type=float, nargs='?', const=-1)
+parser.add_argument("--N", type=int, nargs='?', const=-1)
 args = parser.parse_args()
 opt = str(args.opt)
 arch = str(args.arch)
+attack = str(args.attack)
 
 inference = opt
 
@@ -41,25 +45,38 @@ from tqdm import trange
 
 loss = tf.keras.losses.SparseCategoricalCrossentropy()
 
+if(attack == "FGSM"):
+    meth = analyzers.FGSM
+elif(attack == "PGD"):
+    meth = analyzers.PGD
+elif(attack == "CW"):
+    meth = analyzers.CW
+elif(attack == 'GA'):
+    meth = analyzers.cifar_gen_attack
+
+num_images = 100
 
 #print(model._predict(X_test[0:1]))
 #print(analyzers.IBP(model, X_test[0:1], eps=0.0, weights=model.model.get_weights(), predict=True))
 accuracy = tf.keras.metrics.Accuracy()
-preds = model.predict( X_test[0:250])
-accuracy.update_state(np.argmax(preds, axis=1), y_test[0:250])
+preds = model.predict( X_test[0:num_images])
+accuracy.update_state(np.argmax(preds, axis=1), y_test[0:num_images])
 print("Accuracy: ",  accuracy.result())
 accuracy = tf.keras.metrics.Accuracy()
-adv = analyzers.FGSM(model, X_test[0:250], eps=0.007, loss_fn=loss, num_models=5)
+if attack == 'GA':
+    adv = meth(model, X_test[0:num_images], G=args.G, R=args.R, N=args.N, D=0.007)
+else:
+    adv = meth(model, X_test[0:num_images], eps=0.007, loss_fn=loss, num_models=5)
 preds = model.predict(adv) #np.argmax(model.predict(np.asarray(adv)), axis=1)
-accuracy.update_state(np.argmax(preds, axis=1), y_test[0:250])
+accuracy.update_state(np.argmax(preds, axis=1), y_test[0:num_images])
 fgsm = accuracy.result()
-print("FGSM Robustness: ", accuracy.result())
+print(attack + " Robustness: ", accuracy.result())
 
 accuracy = tf.keras.metrics.Accuracy()
-preds = analyzers.chernoff_bound_verification(model, X_test[0:250], 0.0035, y_test[0:250], confidence=0.90)
+preds = analyzers.chernoff_bound_verification(model, X_test[0:num_images], 0.0035, y_test[0:num_images], confidence=0.90)
 #print(preds.shape)
 #print(np.argmax(preds, axis=1).shape)
-accuracy.update_state(np.argmax(preds, axis=1), y_test[0:250])
+accuracy.update_state(np.argmax(preds, axis=1), y_test[0:num_images])
 print("Massart Lower Bound (IBP): ",  accuracy.result())
 
 
